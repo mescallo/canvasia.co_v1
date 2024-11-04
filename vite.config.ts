dde042d4dd18d514b0572bf334a19f79447489ec
mescallo <mescallo@euphoria-net.com>
2024-11-04 00:57:36 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 4173
+  },
+  preview: {
+    host: true,
+    port: 4173
+  }
+})
